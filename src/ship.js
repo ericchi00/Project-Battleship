e8412fe01757949277b890ef4e901e0f3f5e7184
ericchi00 <ericchi00@gmail.com>
2022-03-22 15:25:56 -0700

    updated ship functions

--- src/ship.js
+++ src/ship.js
@@ -1,10 +1,30 @@
 // ship constructor
-const Ship = (length, hit, sunk) => ({ length, hit, sunk });
+class Ship {
+  constructor(length, hit, sunk) {
+    this.length = length;
+    this.hit = hit;
+    this.sunk = sunk;
+    this.ship = Array.apply(null, Array(5)).map(function (x, i) {
+      return i;
+    });
+  }
+}
 
+
+// marks ship array to null to indicate it is hit
 Ship.prototype.hit = (number) => {
-  // takes in number and marks it as hit
+    this.ship[number] = null;
 };
 
-Ship.prototype.isSunk = (length, hit) => {
-  // calculates if ship is sunk based on length and whether all of their positions are hit
+
+// checks if the entire ship array is null, then it is sunk
+Ship.prototype.isSunk = (length) => {
+    let numOfHit = 0;
+    for (let i = 0; i <= this.ship.length; i++) {
+        if (this.ship[i] === null) {
+            numOfHit += 1;
+        }
+    }
+    if (numOfHit === length) return true;
+    else return false;
 };
