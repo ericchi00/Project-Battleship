1fcb49a693fe3803387ea6dc82f6448d38813aa0
ericchi00 <ericchi00@gmail.com>
2022-03-25 17:16:07 -0700

    fixed array function for Ship size

--- src/ship.js
+++ src/ship.js
@@ -4,27 +4,25 @@ class Ship {
     this.length = length;
     this.hit = hit;
     this.sunk = sunk;
-    this.ship = Array.apply(null, Array(5)).map(function (x, i) {
-      return i;
-    });
+    this.size = Array.from({ length: length }, (value, num) => num);
   }
 }
 
-
 // marks ship array to null to indicate it is hit
 Ship.prototype.hit = (number) => {
-    this.ship[number] = null;
+  this.ship[number] = null;
 };
-
 
 // checks if the entire ship array is null, then it is sunk
 Ship.prototype.isSunk = (length) => {
-    let numOfHit = 0;
-    for (let i = 0; i <= this.ship.length; i++) {
-        if (this.ship[i] === null) {
-            numOfHit += 1;
-        }
+  let numOfHit = 0;
+  for (let i = 0; i <= this.ship.length; i++) {
+    if (this.ship[i] === null) {
+      numOfHit += 1;
     }
-    if (numOfHit === length) return true;
-    else return false;
+  }
+  if (numOfHit === length) return true;
+  else return false;
 };
+
+export default Ship;
