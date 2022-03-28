a8dd3f41128174d39f8a14f550271128a68f4d84
ericchi00 <ericchi00@gmail.com>
2022-03-28 13:23:26 -0700

    updated tests after changing constructors

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,11 +1,11 @@
 import Ship from "./ship";
 
 test("Dynamically create array based on input", () => {
-  const newShip = new Ship(5, 0, false);
+  const newShip = new Ship(5);
   expect(newShip.size.length).toBe(5);
 });
 
 test("Check contents of dynamically created array", () => {
-  const newShip = new Ship(5, 0, false);
-  expect(newShip.size).toStrictEqual([0, 1, 2, 3, 4]);
+  const newShip = new Ship(4);
+  expect(newShip.size).toStrictEqual([0, 1, 2, 3]);
 });
