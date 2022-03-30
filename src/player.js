3cdbb5a559cad31bd302e4396f9facd27bdddd5b
ericchi00 <ericchi00@gmail.com>
2022-03-30 10:35:01 -0700

    added base layout for player & computer obj

---
+++ src/player.js
@@ -0,0 +1,16 @@
+let playerTurn = true;
+
+const Player = () => {
+  // after turn, set playerTurn to false
+};
+
+const Computer = () => {
+  function computerTurn() {
+    if (playerTurn === true) return;
+    else {
+      // hit randomly
+    }
+  }
+};
+
+export { playerTurn, Player, Computer };
