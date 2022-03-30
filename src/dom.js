62cdf78969f19822a43da154412a3a15ed47149f
ericchi00 <ericchi00@gmail.com>
2022-03-30 13:02:24 -0700

    changed coord naming to row / col

--- src/dom.js
+++ src/dom.js
@@ -18,33 +18,12 @@ function createComputerBoard() {
 
 function addCoords() {
   const boxes = document.querySelectorAll(".box");
-  const letter = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-  ];
+  const row = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   let num = 0;
   let index = 0;
   for (let i = 0; i < boxes.length; i++) {
-    boxes[i].setAttribute("letter", letter[index]);
-    boxes[i].setAttribute("num", num);
+    boxes[i].setAttribute("row", row[index]);
+    boxes[i].setAttribute("col", num);
     num += 1;
     if (num === 10) {
       num = 0;
