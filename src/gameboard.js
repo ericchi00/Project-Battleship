cc9205c3cd4bc1fc047dd77b03d5562819c89e84
ericchi00 <ericchi00@gmail.com>
2022-03-30 15:21:13 -0700

    changed gameboard factory to class

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,18 +1,30 @@
 import Ship from "./ship";
 
-const Gameboard = () => {
-  let missed = [];
-  const carrier = new Ship(5);
-  const battleship = new Ship(4);
-  const destroyer = new Ship(3);
-  const submarine = new Ship(3);
-  const patrolboat = new Ship(2);
-  let ships = [carrier, battleship, destroyer, submarine, patrolboat];
-
-    function placeShips(coords) {
-      
+class Gameboard {
+  constructor() {
+    this.missed = [];
+    this.carrier = new Ship(5);
+    this.battleship = new Ship(4);
+    this.destroyer = new Ship(3);
+    this.submarine = new Ship(3);
+    this.patrolboat = new Ship(2);
+    this.ships = [
+      this.carrier,
+      this.battleship,
+      this.destroyer,
+      this.submarine,
+      this.patrolboat,
+    ];
   }
-  function receiveAttack(coords) {
+  placeShips(row, col, ship) {
+    if (document.querySelector(`[row="${row + ship.length}"]`) === null) return;
+    if (document.querySelector(`[col="${col + ship.length}"]`) === null) return;
+    else {
+      const firstBox = document.querySelector(`[row="${row}"][col="${col}"]`);
+      return firstBox;
+    }
+  }
+  receiveAttack(row, col) {
     // add ship class to dom
     if (coords === "ship") {
       // grab the id (type ofship)
@@ -21,7 +33,7 @@ const Gameboard = () => {
       missed.push(coords);
     }
   }
-  function allSunk(shipsArr) {
+  allSunk(shipsArr) {
     let sunk = 0;
     shipsArr.forEach((ship) => {
       if (ship.isSunk === true) sunk += 1;
@@ -29,13 +41,10 @@ const Gameboard = () => {
     if (sunk === 5) return true;
     else return false;
   }
-    
-    function displayMissed() {
-        missed.forEach(coord => {
-            // add missed class to DOM
-        })
-    }
-  return { ships, allSunk };
-};
-
+  displayMissed() {
+    missed.forEach((coord) => {
+      // add missed class to DOM
+    });
+  }
+}
 export default Gameboard;
