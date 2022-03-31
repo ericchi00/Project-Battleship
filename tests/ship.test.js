12c4d4f2e1c10b38be775f2bb76c2d0b1a2200f5
ericchi00 <ericchi00@gmail.com>
2022-03-30 17:49:42 -0700

    updated tests

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,23 +1,28 @@
 import Ship from "../src/ship";
 
 test("Dynamically create array based on input", () => {
-  const newShip = new Ship(5);
+  const newShip = new Ship("test", 5);
   expect(newShip.size.length).toBe(5);
 });
 
 test("Check contents of dynamically created array", () => {
-  const newShip = new Ship(4);
+  const newShip = new Ship("test", 4);
   expect(newShip.size).toStrictEqual([0, 1, 2, 3]);
 });
 
+test("Check contents of dynamically created array", () => {
+  const newShip = new Ship("test", 2);
+  expect(newShip.size).toStrictEqual([0, 1]);
+});
+
 test("check hit function", () => {
-  const newShip = new Ship(4);
+  const newShip = new Ship("test", 4);
   newShip.hit(4);
   expect(newShip.size[4]).toBe(null);
 });
 
-test.only("check sunk function", () => {
-  const newShip = new Ship(2);
+test("check sunk function", () => {
+  const newShip = new Ship("test", 2);
   newShip.hit(0);
   newShip.hit(1);
   expect(newShip.isSunk()).toBe(true);
