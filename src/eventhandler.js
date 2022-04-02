88ec55662017e7de86529a393096b772ec047eec
ericchi00 <ericchi00@gmail.com>
2022-04-01 18:06:14 -0700

    moved all boat functions into one mega func

--- src/eventhandler.js
+++ src/eventhandler.js
@@ -1,85 +1,44 @@
 import Gameboard from "./gameboard";
 
-const playerBoard = new Gameboard("player");
-const board = document.querySelector(".playerBoard");
-
-function addPatrolboat() {
-  board.addEventListener("click", function handler(e) {
-    const row = Number(e.target.getAttribute("row"));
-    const col = Number(e.target.getAttribute("col"));
-    playerBoard.placeShips(row, col, playerBoard.patrolboat, "x");
-    if (
-      board
-        .querySelector(`[row="${row}"][col="${col}"]`)
-        .getAttribute("ship") === "patrolboat"
-    ) {
-      board.removeEventListener("click", handler);
-    }
-  });
+function containsShip(ship) {
+  const boxes = document.querySelector(".playerBoard").querySelectorAll(".box");
+  for (let i = 0; i < boxes.length; i += 1) {
+    if (boxes[i].getAttribute("ship") === ship.name) return true;
+  }
+  return false;
 }
 
-function addSubmarine() {
-  board.addEventListener("click", function handler(e) {
-    const row = Number(e.target.getAttribute("row"));
-    const col = Number(e.target.getAttribute("col"));
-    playerBoard.placeShips(row, col, playerBoard.submarine, "x");
-    if (
-      board
-        .querySelector(`[row="${row}"][col="${col}"]`)
-        .getAttribute("ship") === "submarine"
-    ) {
-      board.removeEventListener("click", handler);
-      addPatrolboat();
-    }
-  });
+function changeCursor() {
+  const boxes = Array.from(
+    document.querySelector(".playerBoard").querySelectorAll(".box")
+  );
+  for (let i = 0; i < boxes.length; i += 1) {
+    boxes[i].classList.add("noTouch");
+  }
 }
 
-function addDestroyer() {
-  board.addEventListener("click", function handler(e) {
-    const row = Number(e.target.getAttribute("row"));
-    const col = Number(e.target.getAttribute("col"));
-    playerBoard.placeShips(row, col, playerBoard.destroyer, "x");
-    if (
-      board
-        .querySelector(`[row="${row}"][col="${col}"]`)
-        .getAttribute("ship") === "destroyer"
-    ) {
-      board.removeEventListener("click", handler);
-      addSubmarine();
-    }
-  });
-}
-
-function addBattlesship() {
-  board.addEventListener("click", function handler(e) {
-    const row = Number(e.target.getAttribute("row"));
-    const col = Number(e.target.getAttribute("col"));
-    playerBoard.placeShips(row, col, playerBoard.battleship, "x");
-    if (
-      board
-        .querySelector(`[row="${row}"][col="${col}"]`)
-        .getAttribute("ship") === "battleship"
-    ) {
-      board.removeEventListener("click", handler);
-      addDestroyer();
-    }
-  });
-}
-
-// starts function loop and adds & removes event listeners
 function addBoats() {
-  board.addEventListener("click", function handler(e) {
+  const playerBoard = new Gameboard("player");
+  const board = document.querySelector(".playerBoard");
+  board.addEventListener("click", function listener(e) {
     const row = Number(e.target.getAttribute("row"));
     const col = Number(e.target.getAttribute("col"));
-    playerBoard.placeShips(row, col, playerBoard.carrier, "x");
-    if (
-      board
-        .querySelector(`[row="${row}"][col="${col}"]`)
-        .getAttribute("ship") === "carrier"
-    ) {
-      board.removeEventListener("click", handler);
-      addBattlesship();
+    if (containsShip(playerBoard.carrier)) {
+      if (containsShip(playerBoard.battleship)) {
+        if (containsShip(playerBoard.destroyer)) {
+          if (containsShip(playerBoard.submarine)) {
+            if (playerBoard.placeShips(row, col, playerBoard.patrolboat, "y")) {
+              board.removeEventListener("click", listener);
+              changeCursor();
+            }
+          }
+          playerBoard.placeShips(row, col, playerBoard.submarine, "y");
+        }
+        playerBoard.placeShips(row, col, playerBoard.destroyer, "y");
+      }
+      playerBoard.placeShips(row, col, playerBoard.battleship, "x");
     }
+    playerBoard.placeShips(row, col, playerBoard.carrier, "y");
   });
 }
 
