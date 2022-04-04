cdbf540dde367b18340f108a884b83baa4def614
ericchi00 <ericchi00@gmail.com>
2022-04-04 15:51:16 -0700

    added comments & finished logic for takeTurns

--- src/eventhandler.js
+++ src/eventhandler.js
@@ -1,5 +1,3 @@
-import Gameboard from "./gameboard";
-
 function containsShip(ship) {
   const boxes = document.querySelector(".playerBoard").querySelectorAll(".box");
   for (let i = 0; i < boxes.length; i += 1) {
@@ -8,53 +6,99 @@ function containsShip(ship) {
   return false;
 }
 
+// once noTouch is added, cursor changes to normal on player.board
 function changeCursor() {
-  const boxes = Array.from(
-    document.querySelector(".playerBoard").querySelectorAll(".box")
-  );
+  const boxes = Array.from(document.querySelectorAll(".box"));
   for (let i = 0; i < boxes.length; i += 1) {
-    boxes[i].classList.add("noTouch");
+    boxes[i].classList.add("setup");
   }
 }
 
 let axis = "x";
 function rotateAxis() {
   const button = document.querySelector("#axis");
+  const current = document.querySelector(".currentAxis");
   button.addEventListener("click", () => {
     if (axis === "x") {
-      button.textContent = "Rotate Axis to Y";
+      button.textContent = "Change to X";
       axis = "y";
+      current.textContent = "Current Axis: Y";
     } else if (axis === "y") {
-      button.textContent = "Rotate Axis to X";
+      button.textContent = "Change to Y";
       axis = "x";
+      current.textContent = "Current Axis: X";
     }
   });
 }
 
-function addBoats() {
-  rotateAxis();
-  const playerBoard = new Gameboard("player");
+// lets player attack on Computer Board
+function takeTurns(player, computer) {
+  const computerBoard = document.querySelector(".computerBoard");
+  const instructions = document.querySelector(".instructions");
+  const win = document.createElement("p");
+  computerBoard.addEventListener("click", function listener(e) {
+    const row = Number(e.target.getAttribute("row"));
+    const col = Number(e.target.getAttribute("col"));
+    // check if player's ships are all sunk for computer win
+    if (player.board.allSunk()) {
+      while (instructions.firstChild) {
+        instructions.removeChild(instructions.lastChild);
+      }
+      win.textContent = "Computer wins!";
+      instructions.appendChild(win);
+      computerBoard.removeEventListener("click", listener);
+      return;
+    }
+    // if receiveAttack returns false, it's already been hit
+    if (!computer.board.receiveAttack(row, col)) return;
+    // check if computer is attacking the same coords again
+    let computerAttack = computer.randomAttack();
+    while (!player.board.receiveAttack(computerAttack[0], computerAttack[1])) {
+      // keeps rerolling computerAttack until it hits (true), then checks for win, if not it cancels the loop
+      computerAttack = computer.randomAttack();
+      // checks if computer wins
+      if (computer.board.allSunk()) {
+        while (instructions.firstChild) {
+          instructions.removeChild(instructions.lastChild);
+        }
+        win.textContent = "You win!";
+        instructions.appendChild(win);
+        computerBoard.removeEventListener("click", listener);
+        return;
+      }
+      if (player.board.receiveAttack(computerAttack[0], computerAttack[1])) {
+        return;
+      }
+    }
+  });
+}
+
+function addBoats(player, computer) {
   const board = document.querySelector(".playerBoard");
   board.addEventListener("click", function listener(e) {
     const row = Number(e.target.getAttribute("row"));
     const col = Number(e.target.getAttribute("col"));
-    if (containsShip(playerBoard.carrier)) {
-      if (containsShip(playerBoard.battleship)) {
-        if (containsShip(playerBoard.destroyer)) {
-          if (containsShip(playerBoard.submarine)) {
-            if (playerBoard.placeShips(row, col, playerBoard.patrolboat, axis)) {
+    if (containsShip(player.board.carrier)) {
+      if (containsShip(player.board.battleship)) {
+        if (containsShip(player.board.destroyer)) {
+          if (containsShip(player.board.submarine)) {
+            if (
+              player.board.placeShips(row, col, player.board.patrolboat, axis)
+            ) {
+              // removes ability to add more boats, changes cursor, and lets you click on computer board
               board.removeEventListener("click", listener);
               changeCursor();
+              takeTurns(player, computer);
             }
           }
-          playerBoard.placeShips(row, col, playerBoard.submarine, axis);
+          player.board.placeShips(row, col, player.board.submarine, axis);
         }
-        playerBoard.placeShips(row, col, playerBoard.destroyer, axis);
+        player.board.placeShips(row, col, player.board.destroyer, axis);
       }
-      playerBoard.placeShips(row, col, playerBoard.battleship, axis);
+      player.board.placeShips(row, col, player.board.battleship, axis);
     }
-    playerBoard.placeShips(row, col, playerBoard.carrier, axis);
+    player.board.placeShips(row, col, player.board.carrier, axis);
   });
 }
 
-export default addBoats;
+export { addBoats, rotateAxis };
